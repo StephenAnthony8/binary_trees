a145d5887d1814dfbb346416f23bd35ddbf1e7eb
GLetiwa <graceletiwa@gmail.com>
2023-11-28 16:52:49 +0000

    2nd attempt

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,15 +10,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
+
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-	{
-		return (left_height + 1);
-	}
-	else
-	{
-		return (right_height + 1);
-	}
+	return ((left_height > right_height) ? left_height :
+			right_height);
 }
