52e907ce89a7f33c6515f721752fafbb1d5de061
GLetiwa <graceletiwa@gmail.com>
2023-11-28 15:38:53 +0000

    first

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,7 +4,7 @@
  * binary_tree_is_root - checks if a node is a root
  * @node: pointer to the node in check
  *
- * Return 1 if node is a root, 0 otherwise
+ * Return: 1 if node is a root, 0 otherwise
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
