027b0aa3368b3959f32c24923acfd171ce359784
GLetiwa <graceletiwa@gmail.com>
2023-11-28 15:54:55 +0000

    first

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,15 +8,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	size_t left_height = binary_tree_height(tree->left); 
-	size_t right_height = binary_tree_height(tree->right); 
-	
-	if (left_height > right_height) 
-	{ 
-		return left_height + 1; 
-	} 
-	else 
-	{ 
-		return right_height + 1; 
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
 	}
 }
