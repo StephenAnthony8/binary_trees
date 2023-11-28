cd110d92b2f48d5c0af9bda874308171b331802c
GLetiwa <graceletiwa@gmail.com>
2023-11-28 17:10:23 +0000

    2nd attempt

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - calculates the size of a bt
+ * @tree: pointer to the root node of the bt
+ *
+ * Return: size of the bt
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	size_t left_size = binary_tree_size(tree->left);
+	size_t right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
+}
